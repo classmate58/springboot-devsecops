0a60c49742e750f31e04beb0842762e945be60d7
classmate58 <classmate202658@gmail.com>
2026-07-23 17:29:06 +0530

    Initial Spring Boot DevSecOps project

---
+++ src/main/java/springboot_devsecops/SpringbootDevsecopsApplication.java
@@ -0,0 +1,13 @@
+package springboot_devsecops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevsecopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDevsecopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springboot_devsecops/SpringbootDevsecopsApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot_devsecops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDevsecopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
